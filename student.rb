2dc24b881af2f1d56b72c1a8a44df093dda5d921
williammacleod <williammacleod@me.com>
2014-10-17 09:35:24 -0400

    final commit of student.rb added method favorite_number

--- student.rb
+++ student.rb
@@ -8,9 +8,13 @@ class Student
     puts "Hi #{target}, I'm #{first_name}!"
 
   end
+  def favorite_number
+    7
+  end
 end
 
 
 frank = Student.new
 frank.first_name = "Frank"
 frank.introduction('Katrina')
+puts "Frank's favorite number is #{frank.favorite_number}."
